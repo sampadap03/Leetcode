2b4a4f4b28363e6c0b8f64c7f167473fa6572c5e
Sampada Petkar <40736070+sampadap03@users.noreply.github.com>
2020-09-21 17:29:56 +0530

    Add problem statement

--- Top Interview Questions/Contains-Duplicate.py
+++ Top Interview Questions/Contains-Duplicate.py
@@ -3,6 +3,7 @@
 # Languagee : Python3
 # Question: https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/578
 # Contains Duplicate
+# Statement: Given an array of integers, find if the array contains any duplicates. Your function should return true if any value appears at least twice in the array, and it should return false if every element is distinct.
 
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
