bbec3f7fa4b3b2949391733c2e2a930d3d26a921
Sampada Petkar <40736070+sampadap03@users.noreply.github.com>
2020-09-21 17:32:20 +0530

    Add problem statement

--- Top Interview Questions/Remove-duplicates-from-sorted-array.py
+++ Top Interview Questions/Remove-duplicates-from-sorted-array.py
@@ -3,6 +3,7 @@
 # Language : Python3
 # Question: https://leetcode.com/explore/featured/card/top-interview-questions-easy/92/array/727/
 # Remove duplicates from sorted array
+# Statement: Given a sorted array nums, remove the duplicates in-place such that each element appear only once and return the new length. Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
 
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:  
