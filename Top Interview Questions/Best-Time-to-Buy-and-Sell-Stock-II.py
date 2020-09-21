ace4c0801cc5db3185423b726774057beb747fdf
Sampada Petkar <40736070+sampadap03@users.noreply.github.com>
2020-09-21 17:31:34 +0530

    Add problem statement

--- Top Interview Questions/Best-Time-to-Buy-and-Sell-Stock-II.py
+++ Top Interview Questions/Best-Time-to-Buy-and-Sell-Stock-II.py
@@ -3,6 +3,7 @@
 # Language : Python3
 # Question : https://leetcode.com/explore/featured/card/top-interview-questions-easy/92/array/564
 # Best Time to Buy and Sell Stock II
+# Statement: Say you have an array prices for which the ith element is the price of a given stock on day i. Design an algorithm to find the maximum profit. You may complete as many transactions as you like (i.e., buy one and sell one share of the stock multiple times).
 
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
